74ef58307a966cfa77185df26ecf7a9f949fef53
Leonhard Horstmeyer <leo.h@hotmail.de>
2025-06-18 23:32:40 +0300

    adjusted the text

--- frontend/src/templates/ComingSoon.tsx
+++ frontend/src/templates/ComingSoon.tsx
@@ -23,13 +23,17 @@ export default function ComingSoon() {
       {/* Foreground Content with White Border */}
       <div className="border-white border-[3pt] p-8 rounded-lg z-10 bg-black">
         <h1 className="text-4xl font-bold mb-6 text-center">ENTER RAA SPACE</h1>
-
-        <button
-          onClick={handleEnter}
-          className="px-6 py-3 border border-[#f5f5dc] bg-transparent text-[#f5f5dc] font-semibold rounded-full hover:bg-[#f5f5dc] hover:text-black transition"
-        >
-          ENTER
-        </button>
+        <p className="text-center mb-6 max-w-3xl mx-auto">
+        RAA is a pop-up venue that hosts regular theatrical and contemporary performances, exhibitions and workshops. A little herbal bar offers the possibility to take in some Latvian forest in the centre of Riga.
+        </p>
+        <div className="flex justify-center">
+          <button
+            onClick={handleEnter}
+            className="px-6 py-3 border border-[#f5f5dc] bg-transparent text-[#f5f5dc] font-semibold rounded-full hover:bg-[#f5f5dc] hover:text-black transition"
+          >
+            ENTER
+          </button>
+        </div>
       </div>
     </div>
   );
